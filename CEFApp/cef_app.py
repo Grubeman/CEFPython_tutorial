91929748b4f81fca93559cf18f6bec63314aa600
Grubeman <sebastien.tisseyre0016@orange.fr>
2020-04-14 18:52:35 +0200

    Jinja Env Integration

--- CEFApp/cef_app.py
+++ CEFApp/cef_app.py
@@ -2,15 +2,40 @@
 Base CEF Python Application
 """
 import sys
+import os
 from cefpython3 import cefpython as cef
-
+from urllib.parse import urlparse
 
 class LoadHandler(object):
+    def __init__(self, app=None):
+        self.app = app
+
     def OnLoadStart(self, browser, frame, **_):
         print("On load start")
 
     def OnLoadError(self, browser, frame, error_code, error_text_out, failed_url, **_):
-        print("On load error")
+        parsed_url = urlparse(failed_url)
+        root_path = parsed_url.netloc+parsed_url.path
+        raw_query = parsed_url.query
+        print(parsed_url.query.split("="))
+        query_data = dict([parsed_url.query.split("=")])
+        if 'ERR_UNKNOWN_URL_SCHEME' in error_text_out and parsed_url.scheme == "jinja":
+            try:
+                t = self.app.jinja_env.get_template(root_path)
+                self.app.LoadJinjaUrl(root_path, context=query_data)
+            except Exception as e:
+                print("Template not found",path)
+                pass
+            return
+        if 'ERR_FILE_NOT_FOUND' in error_text_out:
+            root_path = os.path.relpath(root_path, self.app.cef_temp_dir).replace(os.path.sep, "/")
+            try:
+                t = self.app.jinja_env.get_template(root_path)
+                self.app.LoadJinjaUrl(root_path, context=query_data)
+            except Exception as e:
+                print("Template not found",root_path)
+                pass
+            return
         
     def OnLoadEnd(self, browser, frame, **_):
         print("On load end")
@@ -24,7 +49,7 @@ class LoadHandler(object):
             print("DOM is loading")
 
 class CEFApp:
-    def __init__(self, start_url, window_title = "CEFApp"):
+    def __init__(self, start_url, window_title = "CEFApp", jinja_env=None, cef_temp_dir=None):
         """Initialize a new base CEF app
         
         :param start_url: Load url on browser startup
@@ -34,16 +59,20 @@ class CEFApp:
         """
         sys.excepthook = cef.ExceptHook
         cef.Initialize()
+        self.jinja_env = jinja_env
+        if cef_temp_dir is None:
+            self.cef_temp_dir = os.path.join(os.path.dirname(__file__), ".cef_temp")
+        else:
+            self.cef_temp_dir = cef_temp_dir
         self.browser = cef.CreateBrowserSync(url=start_url, window_title=window_title)
         self.bindings = cef.JavascriptBindings(
                     bindToFrames=False, bindToPopups=False)
-        self.set_javascript_bindings()
 
         #Ajout des clients handler
         self.set_client_handlers()
 
     def set_client_handlers(self):
-        client_handlers = [LoadHandler()]
+        client_handlers = [LoadHandler(self)]
         for handler in client_handlers:
             self.browser.SetClientHandler(handler)
 
@@ -52,6 +81,18 @@ class CEFApp:
         """
         self.bindings.SetProperty("py_print", print)
         self.browser.SetJavascriptBindings(self.bindings)
+
+    def LoadJinjaUrl(self, template_url, context=None, **kwargs):
+        if context is None:
+            context = {}
+        t = self.jinja_env.get_template(template_url)
+        if not os.path.exists(self.cef_temp_dir):
+            os.makedirs(self.cef_temp_dir)
+        with open(os.path.join(self.cef_temp_dir, "temp.html"), "w") as f:
+            f.write(t.render(context))
+
+        template_url = os.path.join(self.cef_temp_dir, "temp.html")
+        return self.browser.LoadUrl(template_url, **kwargs)
 
     def run(self):
         cef.MessageLoop()
--- CEFApp/main.py
+++ CEFApp/main.py
@@ -5,7 +5,18 @@ import os
 
 from cef_app import CEFApp
 
+from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+
 S_CURRENT_DIR = os.path.dirname(__file__) #repertoire courant
 
+file_loader = FileSystemLoader(os.path.join(S_CURRENT_DIR, "templates"))
+JINJA_ENV = Environment(loader=file_loader)
+print(JINJA_ENV.list_templates())
+class MyApp(CEFApp):
+
+    def __init__(self):
+        CEFApp.__init__(self, "jinja://"+"helloworld.jinja2?name=Grubeman" ,window_title="MyApp", jinja_env = JINJA_ENV)
+
 if __name__ == "__main__":
-    CEFApp("file://"+os.path.join(S_CURRENT_DIR, "helloworld.html")).run()
+    MyApp().run()
